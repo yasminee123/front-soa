7d73060a78f626fa7472aa38b1509f6b0537371c
yasmine123 <yasminegoubantini6@gmail.com>
2020-12-23 21:14:56 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { pathToFileURL } from 'url';
+import { AddCandidatComponent } from './components/candidat/add-candidat/add-candidat.component';
+import { ListeCandidatsComponent } from './components/candidat/liste-candidats/liste-candidats.component';
+import { UpdateCandidatComponent } from './components/candidat/update-candidat/update-candidat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'candidat',
+  component:ListeCandidatsComponent
+  },
+  
+  {
+    path:'add-candidat',
+    component:AddCandidatComponent
+  },
+
+  {
+    path:'update-candidat/:id',
+    component:UpdateCandidatComponent
+  }
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeCandidatsComponent } from './components/candidat/liste-candidats/liste-candidats.component';
+import { AddCandidatComponent } from './components/candidat/add-candidat/add-candidat.component';
+import { UpdateCandidatComponent } from './components/candidat/update-candidat/update-candidat.component';
+import { ListeElecteursComponent } from './components/electeur/liste-electeurs/liste-electeurs.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeCandidatsComponent,
+    AddCandidatComponent,
+    UpdateCandidatComponent,
+    ListeElecteursComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
